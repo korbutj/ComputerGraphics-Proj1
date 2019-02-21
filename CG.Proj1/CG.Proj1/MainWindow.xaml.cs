96c91d5a8e3bfa41decdcc85e05baec875aea196
Jarosław Korbut <korbutj@student.mini.pw.edu.pl>
2019-02-21 16:40:11 +0100

    Init

---
+++ CG.Proj1/CG.Proj1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CG.Proj1.ViewModels;
+
+namespace CG.Proj1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel ViewModel => (MainWindowViewModel) this.DataContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ CG.Proj1/CG.Proj1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Prism.Mvvm;
+
+namespace CG.Proj1.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+
+    }
+}
